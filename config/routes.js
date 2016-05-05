5879f4ae332bedd9a81afba0bb9c979d319203c6
Rishi Agarwal <rishiag.iitd@gmail.com>
2016-05-05 12:39:49 +0530

    Clearbit Integration

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,21 @@
 'use strict';
 
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var cors = require('cors');
+
+app.use(bodyParser.urlencoded());
+
+var corsOptions = {
+  origin: /^[^.\s]+\.mixmax\.com$/,
+  credentials: true
+};
+
 module.exports = function(app){
-	
+	(function clearbit_route (){
+		var ClearbitCtrl = require('../controllers/clearbit');
+		app.get('/api/clearbit/typeahead', cors(corsOptions), ClearbitCtrl.typeahead);
+		app.get('/api/clearbit/resolver', cors(corsOptions), ClearbitCtrl.resolver);
+	})();
 }
--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,15 @@
 'use strict';
 
-module.exports.values = {
+module.exports = {
     'config': {
         'server': 'http://localhost',
         'port': 8080
+    },
+    "clearbit_key": "84337303a6b732eae103b98728b91798",
+    "icon_links": {
+    	"twitter": "https://cdn0.iconfinder.com/data/icons/flat-social-media-icons-set-round-style-1/550/twitter_2-128.png",
+    	"facebook": "https://cdn0.iconfinder.com/data/icons/flat-social-media-icons-set-round-style-1/550/facebook-128.png",
+    	"linkedin": "https://cdn0.iconfinder.com/data/icons/flat-social-media-icons-set-round-style-1/550/linkedin-128.png",
+    	"googleplus": "https://cdn0.iconfinder.com/data/icons/flat-social-media-icons-set-round-style-1/550/googleplus-128.png"
     }
 };
